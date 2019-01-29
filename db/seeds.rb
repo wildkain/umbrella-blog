4645539907330fd13fb401e8f253e79c10d914d3
Stanislav <wildkain@gmail.com>
2019-01-29 11:34:31 +0300

    Seed and style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 require 'faker'
-10.times do
-  User.create(username: Faker::Internet.username )
-end
-100.times do
+
+# 100.times do
+#   User.create(username: Faker::Internet.username)
+# end
+#
+# 50.times do
+#   UserIp.create(ip: Faker::Internet.ip_v4_address)
+# end
+
+
+200000.times do
   Posts::Handler.call(title: Faker::Lorem.sentence(10),
-                      body:  Faker::Lorem.sentence(100),
-                      username: User.find(rand(1..10)).username,
-                      ip: "192.168.0.#{rand(0..50)}"
-                      )
+                        body:  Faker::Lorem.sentence(100),
+                        username: User.all.sample.username,
+                        ip: UserIp.all.sample.ip )
 end
